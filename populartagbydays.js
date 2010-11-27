965dc1b1139ef1889c9a28da2f12710be70386f3
Damien Alexandre <mobman02@gmail.com>
2010-11-27 21:55:32 +0100

    Testing external JS

--- populartagbydays.js
+++ populartagbydays.js
@@ -1,8 +1,6 @@
 
-var request_instance = y.rest("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks");
-
+   var request_instance = y.rest("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks");
 
 response.object = request_instance
-.query('name', values['user']).query('api_key', values['api_key'])
-.query('limit', values['limit'])
-.get().response;
+.query('user', inputs['user']).query('api_key', inputs['api_key'])
+.query('limit', inputs['limit']).get().response;
