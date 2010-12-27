5845c368a6560fe2dd74c8e1edfed9b60c1b3725
Damien Alexandre <mobman02@gmail.com>
2010-12-27 14:13:32 +0100

    Formating to XML. Online debug, sry for the spam

--- populartagbydays.js
+++ populartagbydays.js
@@ -7,7 +7,7 @@
 
 var recenttracks = y.query("SELECT * FROM lastfm.recenttracks WHERE user='"+inputs['user']+"' AND api_key='"+inputs['api_key']+"'").results;
 recenttracks = y.xmlToJson(recenttracks);
-response.object = recenttracks.lfm;
+response.object = recenttracks.results;
 
 //var recenttracks = {
 //  "results": {
