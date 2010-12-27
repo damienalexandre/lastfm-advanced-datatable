63c958b3299d9784db3fd762f0bdcfe137a15df8
Damien Alexandre <mobman02@gmail.com>
2010-12-27 12:23:38 +0100

    Formating to XML. Online debug, sry for the spam

--- populartagbydays.js
+++ populartagbydays.js
@@ -211,7 +211,7 @@ for (var day in days_array)
 var returnXml = <days></days>;
 for (var day in days_array)
 {
-  returnXml += <result date={day}>{y.jsonToXml(days_array[day])}</result>;
+  returnXml += <day date={day}>{y.jsonToXml(days_array[day])}</day>;
 }
 
 response.object = returnXml;
